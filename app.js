0cc4a717459283c044e7c9c4aebf9c002b410d32
Soltane <soltanne92@gmail.com>
2020-04-22 13:41:36 -0500

    Adding a POST Route

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/places', placesRoutes); // => /api/places...
+
 app.use((error, req, res, next) => {
   if (res.headerSent) {
-      return next(error);
+    return next(error);
   }
-  res.status(error.code || 500);
-  res.json({message: error.message || 'AN unkown error occurred'});
-})
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
+});
 
 app.listen(5000);
