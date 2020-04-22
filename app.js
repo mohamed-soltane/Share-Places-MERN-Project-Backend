78712538ef2d07f476f2605cee2d8b9e3317fd50
Soltane <soltanne92@gmail.com>
2020-04-22 12:12:54 -0500

    Handling Errors

--- app.js
+++ app.js
@@ -6,5 +6,12 @@ const placesRoutes = require('./routes/places-routes');
 const app = express();
 
 app.use('/api/places', placesRoutes); // => /api/places...
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+      return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({message: error.message || 'AN unkown error occurred'});
+})
 
 app.listen(5000);
